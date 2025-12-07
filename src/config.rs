621cf7b3cd09c5739a0afc8635efb066ff5db4a0
halls1shen <halls1.shen@gmail.com>
2025-12-07 22:16:40 +0300

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -106,8 +106,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rd.idc1.cc"];
+pub const RS_PUB_KEY: &str = "qIPs96DMcRwamcX4pHaNX+JCuwEGN3bCskj2a+K3kVI=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
